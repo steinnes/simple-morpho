2ea717f59c1efce73594fad45bd7896cdf1f39e0
Steinn Eldjárn Sigurðarson <steinn@telsolutus.net>
2013-03-12 22:43:35 +0000

    adding slexer

---
+++ slexer.cc
@@ -0,0 +1,41 @@
+#include <FlexLexer.h>
+
+class SLexer
+{
+private:
+	FlexLexer *l;
+	char *last_str;
+	int last_tok;
+	int PEEKED;
+	
+public:
+	SLexer()
+	{
+		l = new yyFlexLexer();
+	}
+	int advance()
+	{
+		if (PEEKED)
+		{
+			PEEKED = 0;
+			return last_tok;
+		}
+		return l->yylex();
+		
+	}
+	char *text()
+	{
+		if (PEEKED) { return last_str; }
+		if (last_str != NULL)
+			free(last_str); // free due to strdup
+		return l->YYText();
+	}
+	int peek();
+	{
+		PEEKED = 1;
+		last_tok = l->yylex();
+		last_str = strdup(l->YYText());
+		return last_tok;
+	}
+}
+
