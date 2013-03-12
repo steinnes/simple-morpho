1a4937be73a2d9867bb8f6883116059efbe6547c
Steinn Eldjárn Sigurðarson <steinn@telsolutus.net>
2013-03-12 15:49:23 +0000

    creeping along

--- tokens.h
+++ tokens.h
@@ -1,7 +1,12 @@
 #ifndef __TOKENS_H__
 #define __TOKENS_H__
 
-#define INT 1
-#define OP 2
+#define NAME   1
+#define OP     2
+#define VALUE  3
+#define VAR    4
+#define SEP    5
+#define ERROR -1
+
 
 #endif
