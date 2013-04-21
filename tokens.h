22e078bc06bcd4a7559658545a4cad2856dd0a8c
Steinn Eldjárn Sigurðarson <steinn@telsolutus.com>
2013-04-21 15:48:35 +0000

    fixed tokens

--- tokens.h
+++ tokens.h
@@ -1,15 +1,19 @@
 #ifndef __TOKENS_H__
 #define __TOKENS_H__
 
-#define NAME   1
-#define OP     2
-#define INT    3
-#define VAR    4
-#define SEP    5
-#define ERROR -1
+#define NAME   	   1
+#define OP         2
+#define INT        3
+#define VAR    	   4
+#define SEMICOLON  5
 
-#define LPAREN 6
-#define RPAREN 7
+#define LPAREN     6
+#define RPAREN     7
+#define LBRACE     8
+#define RBRACE     9
 
+#define COMMA      10
+
+#define ERROR     -1
 
 #endif
