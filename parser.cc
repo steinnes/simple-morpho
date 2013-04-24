33f3b0b35c5cc64caf35789b6848abc29f764129
Steinn Eldjárn Sigurðarson <steinn@telsolutus.com>
2013-04-24 01:48:10 +0000

    mikið skelfing er hægt að dunda sér við þetta útí hið óendanlega...

--- parser.cc
+++ parser.cc
@@ -82,21 +82,21 @@ int priority(const string &s)
 {
 	switch (s[0])
 	{
-		case '?': return(1);
+/*		case '?': return(1);
 		case '~': return(1);
 		case '^': return(1);
 		case ':': return(2);
 		case '|': return(3);
-		case '&': return(4);
+		case '&': return(4); */
 		case '=': return(5);
 		case '!': return(5);
 		case '>': return(5);
 		case '<': return(5);
-		case '+': return(6);
-		case '-': return(6);
-		case '*': return(7);
-		case '/': return(7);
-		case '%': return(7);
+		case '+': return(7);
+		case '-': return(7);
+		case '*': return(6);
+		case '/': return(6);
+		case '%': return(6);
 	}
 	throw OperatorError("Unknown operator:" + s);
 }
